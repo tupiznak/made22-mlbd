7ee57652cb7d2d327b569cbe4e3f27336a3a71ec
tupiznak <akej-vonavi@mail.ru>
2022-10-18 09:18:06 +0000

    fix volumes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 apt update -y \
 && apt install -y wget build-essential libreadline-gplv2-dev libncursesw5-dev \
      libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev \
+&& mkdir -p /opt/python \
 && cd /opt/python \
 && wget -q https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz \
 && tar xzf Python-3.10.8.tgz \
