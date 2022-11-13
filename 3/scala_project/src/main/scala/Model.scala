fd0d930f243f53c7e89fa73b0d540dadf2c7c1b3
tupiznak <akej-vonavi@mail.ru>
2022-11-13 11:13:00 +0000

    add shift

--- 3/scala_project/src/main/scala/Model.scala
+++ 3/scala_project/src/main/scala/Model.scala
@@ -1,9 +1,16 @@
 import breeze.linalg.{DenseMatrix, inv}
 
 object Model {
-  def fit(X: DenseMatrix[Double], y: DenseMatrix[Double]): DenseMatrix[Double]
-  = (inv(X.t * X) * X.t) * y
+  def fit(X: DenseMatrix[Double], y: DenseMatrix[Double]): DenseMatrix[Double] = {
+    val shiftedX = shiftX(X)
+    (inv(shiftedX.t * shiftedX) * shiftedX.t) * y
+  }
 
-  def predict(W: DenseMatrix[Double], X: DenseMatrix[Double]): DenseMatrix[Double]
-  = X * W
+  def predict(W: DenseMatrix[Double], X: DenseMatrix[Double]): DenseMatrix[Double] = {
+    val shiftedX = shiftX(X)
+    shiftedX * W
+  }
+
+  def shiftX(X: DenseMatrix[Double]): DenseMatrix[Double]
+  = DenseMatrix.horzcat(DenseMatrix.ones[Double](X.rows, 1), X)
 }
--- 3/scala_project/src/test/scala/ModelTest.scala
+++ 3/scala_project/src/test/scala/ModelTest.scala
@@ -3,14 +3,34 @@ import org.scalactic.Tolerance.convertNumericToPlusOrMinusWrapper
 import org.scalatest.funsuite.AnyFunSuite
 
 class ModelTest extends AnyFunSuite {
-  test("Model.fit") {
+
+  test("Model.shift") {
     val X = convert(DenseMatrix(
+      (2, 3, 4),
+      (3, 2, 1),
+      (3, 5, 6),
+      (2, 1, 2),
+      (2, 4, 3),
+    ), Double)
+    val shiftedX = convert(DenseMatrix(
       (1, 2, 3, 4),
       (1, 3, 2, 1),
       (1, 3, 5, 6),
       (1, 2, 1, 2),
       (1, 2, 4, 3),
     ), Double)
+
+    Model.shiftX(X).toArray.lazyZip(shiftedX.toArray).map((x1, x2) => assert(x1 === x2 +- 1e-6))
+  }
+  test("Model.fit_predict") {
+    val X = convert(DenseMatrix(
+      (2, 3, 4),
+      (3, 2, 1),
+      (3, 5, 6),
+      (2, 1, 2),
+      (2, 4, 3),
+    ), Double)
+
     val y = convert(DenseMatrix(
       (7, 2),
       (5, 3),
@@ -24,16 +44,19 @@ class ModelTest extends AnyFunSuite {
       (-1.171875, -0.59375),
       (1.078125, -0.09375),
     )
-    Model.fit(X, y).toArray.lazyZip(W.toArray).map((x, w) => assert(x === w +- 1e-6))
+    val resW = Model.fit(X, y)
+    resW.toArray.lazyZip(W.toArray).map((x, w) => assert(x === w +- 1e-6))
 
     val x_test = convert(DenseMatrix(
-      (1, 3, 5, 6),
-      (1, 3, 2, 8)
+      (3, 5, 6),
+      (3, 2, 8)
     ), Double)
     val y_test = DenseMatrix(
       (6.4375, 0.875),
       (12.109375, 2.46875)
     )
-    Model.predict(W, x_test).toArray.lazyZip(y_test.toArray).map((yp, yt) => assert(yt === yp +- 1e-6))
+    Model.predict(resW, x_test).toArray.lazyZip(y_test.toArray).map(
+      (yp, yt) => assert(yt === yp +- 1e-6)
+    )
   }
 }
